48da0f6ca9d5778f907c07ddd1021040d69305a8
Ashton Carman <ashton.carman@rocketmail.com>
2021-12-06 14:43:31 -0600

    created a scorboard but it doesnt add to itself, and i took out the music part.

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-
+for (i=0;i<=3;i++)
+{
+    game();
+}
+function game(){
 const randomNumber = Math.floor(Math.random() * 100);{
     if (randomNumber <= 33) {
         computerSelection = "rock"
@@ -12,38 +16,57 @@ const randomNumber = Math.floor(Math.random() * 100);{
 }
     console.log(computerSelection);
 
-const music = new Audio('9+10.mp3');
 //create variables named rock, paper, and scissors for both user and computer
 // ask user to input either Rock, Paper, or Scissors
- const userSelection = prompt("Please type 'Rock', 'Paper', or 'Scissors' to play").toLowerCase();
+
+ const userSelection = prompt("Please type 'Rock', 'Paper', or 'Scissors' to play").toLowerCase()
     console.log(userSelection)
 
-    function compare(computerSelection, userSelection){
-
-        //tie
+function compare(computerSelection, userSelection){
+//scoreboard
+    let userScore = 0;
+    computerScore= 0;
+    console.log( userScore , computerScore )
+    
+     //tie
         if (computerSelection === userSelection){
             alert ("Tie! Go again");
         }
-        //winning
+     //winning
         else if (computerSelection === "rock" && userSelection === "paper"){
             alert("The computer chose " + computerSelection + " You Won!");
+            userScore++;
         }
         else if (computerSelection === "paper"  && userSelection === "scissors"){
             alert ("The computer chose " + computerSelection + " You Won!");
+            userScore++;
         }
         else if (computerSelection === "scissors" && userSelection === "rock"){
             alert ("The computer chose " + computerSelection + " You Won!");
+            userScore++;
         }
-        //losing
+    //losing
         else if (computerSelection === "rock" && userSelection === "scissors"){
-            alert ("The computer chose " + computerSelection + "You lost!");
+            alert ("The computer chose " + computerSelection + " You lost!");
+            computerScore++;      
         }
         else if (computerSelection === "paper" && userSelection === "rock"){
-            alert ("The computer chose " + computerSelection + "You Lost!");
+            alert ("The computer chose " + computerSelection + " You Lost!");
+            computerScore++;
         }
-        else music.play();
+        else if (computerSelection === "scissors"  && userSelection === "paper"){
+            alert ("The computer chose " + computerSelection + " You Lost!");
+            computerScore++;
+        }
+        else 
         alert("you have to type one of the 3 things");
+
+        console.log(userScore, computerScore)
     }
     console.log("The computer chose " + computerSelection)
     console.log("You chose " + userSelection)
     compare(userSelection , computerSelection)
+    
+}
+//assign each win or loss a value of 1
+game()
