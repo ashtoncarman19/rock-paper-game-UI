4562c2eb4f1ca99652ab47694d7b49dae752ae26
Ashton Carman <ashton.carman@rocketmail.com>
2021-12-06 11:55:57 -0600

    the game is opperational but you have to refresh it every time to play and the results have to be typed out completely, so im sure theres a way around that but for now at least it works.

--- script.js
+++ script.js
@@ -1,16 +1,44 @@
-function computerPlay(computerSelection){
-    const randomNumber = Math.floor(Math.random() * 100);
+
+const randomNumber = Math.floor(Math.random() * 100);
     if (randomNumber <= 33) {
-        computerSelection = "Rock"
+        computerSelection = "rock"
     }
 
     else if (randomNumber >= 66){
         computerSelection = "paper"
     }
 
-    else computerSelection = "Scissors"
+    else computerSelection = "scissors"
     console.log(computerSelection)
-}
-   computerPlay()
+
 //create variables named rock, paper, and scissors for both user and computer
 // ask user to input either Rock, Paper, or Scissors
+ const userSelection = prompt("Please type 'Rock', 'Paper', or 'Scissors' to play").toLowerCase();
+    console.log(userSelection)
+
+    function compare(computerSelection, userSelection){
+        if (computerSelection === userSelection){
+            alert ("Tie! Go again");
+        }
+        //winning
+        else if (computerSelection === "rock" && userSelection === "paper"){
+            alert("You Won!");
+        }
+        else if (computerSelection === "paper"  && userSelection === "scissors"){
+            alert ("You Won!");
+        }
+        else if (computerSelection === "scissors" && userSelection === "rock"){
+            alert ("You Won!");
+        }
+        //losing
+        else if (computerSelection === "rock" && userSelection === "scissors"){
+            alert ("You lost!");
+        }
+        else if (computerSelection === "paper" && userSelection === "rock"){
+            alert ("You Lost!");
+        }
+        else alert ("Are you sure know how to play?");
+    }
+    console.log("The computer chose " + computerSelection)
+    console.log("You chose " + userSelection)
+    compare(userSelection , computerSelection)
